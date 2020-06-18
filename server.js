84701e5d1c911fa37503865f61988a1d5b9b4522
Dawid Krystochowicz <daw_kryst@interia.pl>
2020-06-18 18:19:17 +0200

    update database

--- server.js
+++ server.js
@@ -1,29 +1,26 @@
 const express = require('express');
 const randomID = require('@rysiek/randomid-generator');
 const app = express();
-const db = [
-  { id: 1, author: 'John Doe', text: 'This company is worth every coin!' },
-  { id: 2, author: 'Amanda Doe', text: 'They really know how to make you happy.' },
-];
+const db = require('./db');
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
 app.get('/testimonials', (req, res) => {
-  res.json(db);
+  res.json(db.testimonials);
 });
 
 app.get('/testimonials/random', (req, res) => {
-  res.json(db[Math.floor(Math.random() + db.length)]);
+  res.json(db.testimonials[Math.floor(Math.random() + db.testimonials.length)]);
 });
 
 app.get('/testimonials/:id', (req, res) => {
-  res.json(db.filter(item => item.id == req.params.id));
+  res.json(db.testimonials.filter(item => item.id == req.params.id));
 });
 
 app.post('/testimonials', (req, res) => {
   const {author, text} = req.body;
-  db.push({
+  db.testimonials.push({
     id: randomID(10),
     author: author,
     text: text,
@@ -33,16 +30,20 @@ app.post('/testimonials', (req, res) => {
 
 app.put('/testimonials/:id', (req, res) => {
   const {author, text} = req.body;
-  const element = db.filter(item => item.id == req.params.id);
+  const element = db.testimonials.filter(item => item.id == req.params.id);
   element[0].author = author;
   element[0].text = text;
   res.json({message: 'OK'});
 });
 
 app.delete('/testimonials/:id', (req, res) => {
-  const element = db.filter(item => item.id == req.params.id);
-  db.splice(db.indexOf(element[0]), 1);
+  const element = db.testimonials.filter(item => item.id == req.params.id);
+  db.splice(db.testimonials.indexOf(element[0]), 1);
   res.json({message: 'OK'});
+});
+
+app.use((req, res) => {
+  res.status(404).json({message: 'Not found...'});
 });
 
 app.listen(8000, () => {
