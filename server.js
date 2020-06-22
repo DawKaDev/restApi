20bf0f16e0e96b18fadacacdf79c42682cd6df20
Dawid Krystochowicz <daw_kryst@interia.pl>
2020-06-22 19:11:27 +0200

    add heroku repository

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const path = require('path');
 /* import routes */
 const testimonialsRoutes = require('./routes/testimonials.routes');
 const concertsRoutes = require('./routes/concerts.routes');
 const seatsRoutes = require('./routes/seats.routes');
+
+app.use(express.static(path.join(__dirname, '/client/build')));
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
@@ -14,10 +17,14 @@ app.use('/api', testimonialsRoutes);
 app.use('/api', concertsRoutes);
 app.use('/api', seatsRoutes);
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/client/build/index.html'));
+});
+
 app.use((req, res) => {
   res.status(404).json({message: 'Not found...'});
 });
 
-app.listen(8000, () => {
+app.listen(process.env.PORT || 8000, () => {
   console.log('Server is running on port: 8000');
 });
