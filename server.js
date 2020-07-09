a7a4e73e2d60e6b275c917ced910c9225b1d511f
Dawid Krystochowicz <daw_kryst@interia.pl>
2020-07-09 17:55:25 +0200

    add mongoose

--- server.js
+++ server.js
@@ -2,13 +2,15 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const path = require('path');
+const mongoose = require('mongoose');
 /* import routes */
 const testimonialsRoutes = require('./routes/testimonials.routes');
 const concertsRoutes = require('./routes/concerts.routes');
 const seatsRoutes = require('./routes/seats.routes');
 const socketIo = require('socket.io');
 const io = socketIo(app.listen(process.env.PORT || 8000));
-
+mongoose.connect('mongodb://localhost:27017/NewWaveDB', {useNewUrlParser: true, useUnifiedTopology: true});
+const db = mongoose.connection;
 app.use(express.static(path.join(__dirname, '/client/build')));
 
 app.use(express.urlencoded({extended: true}));
@@ -36,6 +38,10 @@ io.on('connection', (socket) => {
   console.log('New socket');
 });
 
+db.once('open', () => {
+  console.log('Connected to the database');
+});
+db.on('error', err => console.log('Error ' + err));
 /*app.listen(process.env.PORT || 8000, () => {
   console.log('Server is running on port: 8000');
 });*/
